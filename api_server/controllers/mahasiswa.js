634e90a2c92297f3b3538a6a700c6b93fbba7cc6
ahmfarisi <rachmat.nur91@gmail.com>
2022-10-27 15:14:08 +0700

    Writing and Consuming REST API

--- api_server/controllers/mahasiswa.js
+++ api_server/controllers/mahasiswa.js
@@ -19,19 +19,72 @@ const mhsList = (req, res) => {
 };
 
 const mhsCreate = (req, res) => {
-
+    mahasiswa.create(
+        {
+            npm: req.body.npm,
+            nama: req.body.nama,
+            tgl_lahir: req.body.tgl_lahir,
+            hobi: req.body.hobi
+        }, (err, result) => {
+            if (err) {
+                res
+                .status(400)
+                .json(err);
+            } else {
+                res
+                .status(201)
+                .json(result);
+            }
+        });
 };
 
 const mhsReadOne = (req, res) => {
-
+    mahasiswa
+        .findById(req.params.id)
+        .exec((err, mhs) => {
+            res
+            .status(200)
+            .json(mhs);
+        });
 };
 
 const mhsUpdateOne = (req, res) => {
-
+    mahasiswa
+        .findById(req.params.id)
+        .exec((err, mhs) => {
+            mhs.npm = req.body.npm;
+            mhs.nama = req.body.nama;
+            mhs.tgl_lahir = req.body.tgl_lahir;
+            mhs.hobi = req.body.hobi;
+            mhs.save((err, result) => {
+                if (err) {
+                    res
+                    .status(404)
+                    .json(result);
+                } else {
+                    res
+                    .status(200)
+                    .json(result);
+                }
+            });
+        });
 };
 
 const mhsDeleteOne = (req, res) => {
-
+    mahasiswa
+        .findById(req.params.id)
+        .exec((err, mhs) => {
+            mhs.remove((err, result) => {
+                if (err) {
+                    res
+                    .status(404)
+                    .json(err);
+                }
+                res
+                .status(204)
+                .json(null);
+            });
+        });
 };
 
 module.exports = {
--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -11,4 +11,12 @@ router.route("/mahasiswa")
       .get(ctrlMhs.mhsList)
       .post(ctrlMhs.mhsCreate);
 
+//http://localhost:3000/api/mahasiswa/123 -> get -> get dataa mhs by id
+//http://localhost:3000/api/mahasiswa/123 -> put -> update data mhs
+//http://localhost:3000/api/mahasiswa/123 -> delete -> update data mhs
+router.route("/mahasiswa/:id")
+      .get(ctrlMhs.mhsReadOne)
+      .put(ctrlMhs.mhsUpdateOne)
+      .delete(ctrlMhs.mhsDeleteOne);
+
 module.exports = router;
